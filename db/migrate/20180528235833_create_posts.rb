9fecbf9bf0ea1d747cbcef6af7a67dca95add037
Veronica Kim <vero.a.kim@gmail.com>
2018-05-28 23:58:49 +0000

    automatically backed up by learn

--- db/migrate/20180528235833_create_posts.rb
+++ db/migrate/20180528235833_create_posts.rb
@@ -1,4 +1,9 @@
-class CreatePosts < ActiveRecord::Migration
+class CreatePost < ActiveRecord::Migration[5.1]
   def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :content
+
+    end
   end
 end
